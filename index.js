2978db1a016b69ddbfd3701945e54cd52b6d9091
Jalish Chauhan <jalish@dicot.tech>
2024-04-06 17:07:40 +0530

    add console log in index.js

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     console.log(' / GET');
-    return res.send({status:200,data:`Hello ${req.query.name }`})
+    console.log(req.query.name);
+    return res.send({status:200,message:`Hello ${req.query.name }`})
 })
 
 app.post('/post',(req,res)=>{
     console.log(' / POST');
+    console.log(req.body)
     return res.send({status:200,message:"POST Request"})
 })
 
 
 app.listen(8000,()=>{
-    console.log('Server is listening on PORT 8000')
+    console.log('Server is listening on PORT 8000') 
 })
