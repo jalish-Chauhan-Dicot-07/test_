4aac75b1a8fc5d7cf9143bd24b991dcdf56778eb
Jalish Chauhan <jalish@dicot.tech>
2024-04-06 12:31:50 +0530

    added console log end of testing api

--- testing/testing.js
+++ testing/testing.js
@@ -17,15 +17,15 @@ const testingAPI = async () => {
   const {default: fetch} = await import("node-fetch");
 
     for (const i of api) {
-        console.log(i)
         console.log('\n')
-        const [obj] = Object.values(i);
-        console.log(obj)        
+        const [obj] = Object.values(i);      
         const { param, method } = obj;
         const response = await fetch(
         `https://test-3sz1.onrender.com${Object.keys(i)}?name=${encodeURIComponent(param)}`,
       { method: method }
     );
+
+    console.log('end')
 
     console.log(await response.json())
   }
